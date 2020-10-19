cc4abdb51c6bb1669cd784c40d874f28da51bea4
fanxuankai <fan_xuankai@126.com>
2020-10-19 15:30:13 +0800

    clean

--- canal-boot-autoconfigure/canal-mq-boot-autoconfigure/src/main/java/com/fanxuankai/canal/mq/config/CanalMqAutoConfiguration.java
+++ canal-boot-autoconfigure/canal-mq-boot-autoconfigure/src/main/java/com/fanxuankai/canal/mq/config/CanalMqAutoConfiguration.java
@@ -1,14 +1,12 @@
 package com.fanxuankai.canal.mq.config;
 
 import com.fanxuankai.canal.mq.core.listener.ConsumerHelper;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Import;
 
 /**
  * @author fanxuankai
  */
-@ConditionalOnProperty(prefix = CanalMqProperties.PREFIX, name = "enabled", havingValue = "true")
 @EnableConfigurationProperties(CanalMqProperties.class)
 @Import({AnnotationCanalListenerFactory.class, ConsumerHelper.class})
 public class CanalMqAutoConfiguration {
