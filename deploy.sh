47d2969dd807bcc967d67c26cf6a7abd03c4746d
fanxuankai <fan_xuankai@126.com>
2021-06-22 10:00:58 +0800

    remove flatten

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+cd canal-dependencies
+mvn clean deploy
+cd ../canal-parent
+mvn clean deploy
+cd ../canal-core
+mvn clean deploy
+cd ../canal-db
+mvn clean deploy
+cd ../canal-elasticsearch
+mvn clean deploy
+cd ../canal-mq
+mvn clean deploy
+cd ../canal-redis
+mvn clean deploy
