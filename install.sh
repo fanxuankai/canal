b240e524adeb804b53392ddf3097ea92b95fc7fd
fanxuankai <fan_xuankai@126.com>
2021-10-26 15:20:51 +0800

    clean

---
+++ install.sh
@@ -0,0 +1,14 @@
+cd canal-dependencies
+mvn clean install
+cd ../canal-parent
+mvn clean install
+cd ../canal-core
+mvn clean install
+cd ../canal-db
+mvn clean install
+cd ../canal-elasticsearch
+mvn clean install
+cd ../canal-mq
+mvn clean install
+cd ../canal-redis
+mvn clean install
